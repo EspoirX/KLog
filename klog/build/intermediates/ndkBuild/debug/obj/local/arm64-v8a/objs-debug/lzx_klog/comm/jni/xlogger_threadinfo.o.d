b70e634fe9e7549cfd355d93fcd5608902a93746
Espoir <386707112@qq.com>
2023-09-14 14:54:26 +0800

    更新代码
    更新代码

--- klog/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/lzx_klog/comm/jni/xlogger_threadinfo.o.d
+++
@@ -1,5 +0,0 @@
-/Users/lizixian/Downloads/log/klog/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/lzx_klog/comm/jni/xlogger_threadinfo.o: \
-  /Users/lizixian/Downloads/log/klog/src/main/jni/comm/jni/xlogger_threadinfo.cc \
-  /Users/lizixian/Downloads/log/klog/src/main/jni/comm/compiler_util.h
-
-/Users/lizixian/Downloads/log/klog/src/main/jni/comm/compiler_util.h:
--- klog/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/lzx_klog/main.o.d
+++
@@ -1,2 +0,0 @@
-/Users/lizixian/Downloads/log/klog/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/lzx_klog/main.o: \
-  /Users/lizixian/Downloads/log/klog/src/main/jni/main.cc
